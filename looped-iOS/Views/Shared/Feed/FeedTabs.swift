66fd2b5598b00643b921bd801203ef07df13890a
William Millen <wvmillen@gmail.com>
2025-09-23 16:53:18 -0400

    fixed background

--- looped-iOS/Views/Shared/Feed/FeedTabs.swift
+++ looped-iOS/Views/Shared/Feed/FeedTabs.swift
@@ -81,7 +81,7 @@ struct FeedTabs: View {
                 .padding(.horizontal, 16)
             }
             .padding(.vertical, 8)
-            .background(Color.clear)
+            .background(Color.loopedBackground)
         }
     }
 }
