75e769bf571f3caa700ab1356977b1ab7c2f0b83
William Millen <wvmillen@gmail.com>
2026-02-20 13:11:24 -0500

    small fix

--- looped-iOS/Views/Shared/Navigation/NavigationCanPopReader.swift
+++ looped-iOS/Views/Shared/Navigation/NavigationCanPopReader.swift
@@ -3,13 +3,20 @@ import SwiftUI
 struct NavigationCanPopReader: UIViewControllerRepresentable {
     @Binding var canPop: Bool?
 
+    static func resolveCanPop(from navigationController: UINavigationController?) -> Bool? {
+        guard let navigationController else { return nil }
+        return navigationController.viewControllers.count > 1
+    }
+
     func makeUIViewController(context: Context) -> UIViewController {
         UIViewController()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
         DispatchQueue.main.async {
-            let canPopNow = (uiViewController.navigationController?.viewControllers.count ?? 0) > 1
+            guard let canPopNow = Self.resolveCanPop(from: uiViewController.navigationController) else {
+                return
+            }
             guard canPop != canPopNow else { return }
             canPop = canPopNow
         }
